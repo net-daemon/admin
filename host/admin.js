e927351f85d5968daabb070a92cb5ecbd4e9062f
Ludeeus <ludeeus@ludeeus.dev>
2020-08-16 17:29:22 +0000

    Init

--- host/admin.js
+++ host/admin.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const http = require("http");
 const fetch = require("node-fetch");
+const WebSocket = require("ws");
+const server = require("http").createServer();
 const app = express();
 const path = process.env.ADMINPATH || "";
 const port = 1337;
+const NetDaemonWS = new WebSocket("ws://127.0.0.1:5000");
+
+let wss = new WebSocket.Server({
+  server: server,
+});
 
 app.use(`${path}/`, express.static("build"));
 app.use(`${path}/favicon.png`, express.static("build/favicon.png"));
@@ -55,4 +63,22 @@ app.post(`${path}/api/app/state/:app/enable`, async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Running NetDaemon Admin`));
+let frontendWS;
+
+wss.on("connection", function connection(ws) {
+  frontendWS = ws;
+  ws.on("message", function incoming(message) {
+    const _message = JSON.parse(message);
+    console.log(`received message from frontend:`, _message.type);
+    NetDaemonWS.send(message);
+  });
+});
+
+NetDaemonWS.on("message", function incoming(message) {
+  const _message = JSON.parse(message);
+  console.log(`received message from NetDaemon:`, _message.type);
+  frontendWS.send(message);
+});
+
+server.on("request", app);
+server.listen(port, () => console.log(`Running NetDaemon Admin`));
--- src/data.ts
+++ src/data.ts
@@ -24,44 +24,14 @@ export interface Settings {
   homeAssistantSettings: HomeAssistantSettings;
 }
 
-export const getApps = async () => {
-  const result = await fetch("./api/apps");
-  const apps = (await result.json()) as any;
-  if (apps.errno) {
-    throw new Error(apps.message);
-  }
-  return apps as App[];
+export const requestApps = (webSocket: WebSocket) => {
+  webSocket.send(JSON.stringify({ type: "apps", data: {} }));
 };
 
-export const getSettings = async () => {
-  const result = await fetch("./api/settings");
-  const settings = (await result.json()) as any;
-  if (settings.errno) {
-    throw new Error(settings.message);
-  }
-  return settings as Settings;
+export const requestSettings = (webSocket: WebSocket) => {
+  webSocket.send(JSON.stringify({ type: "settings", data: {} }));
 };
 
-export const enableApp = async (app: string) => {
-  await fetch(`./api/app/state/${app}/enable`, {
-    method: "post",
-  });
-  const result = await fetch("./api/apps");
-  const apps = (await result.json()) as any;
-  if (apps.errno) {
-    throw new Error(apps.message);
-  }
-  return apps as App[];
-};
-
-export const disableApp = async (app: string) => {
-  await fetch(`./api/app/state/${app}/disable`, {
-    method: "post",
-  });
-  const result = await fetch("./api/apps");
-  const apps = (await result.json()) as any;
-  if (apps.errno) {
-    throw new Error(apps.message);
-  }
-  return apps as App[];
+export const appSettings = (webSocket: WebSocket, app: string, data: any) => {
+  webSocket.send(JSON.stringify({ type: "app", app, data }));
 };
