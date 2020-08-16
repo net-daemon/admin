ba53094708c27603abea5c3fbe8dee69b7879ab6
Ludeeus <ludeeus@ludeeus.dev>
2020-08-16 21:46:41 +0000

    Use /api/ws

--- host/admin.js
+++ host/admin.js
@@ -6,7 +6,7 @@ const server = require("http").createServer();
 const app = express();
 const path = process.env.ADMINPATH || "";
 const port = 1337;
-const NetDaemonWS = new WebSocket("ws://127.0.0.1:5000");
+const NetDaemonWS = new WebSocket("ws://127.0.0.1:5000/api/ws");
 
 let wss = new WebSocket.Server({
   server: server,
@@ -14,54 +14,6 @@ let wss = new WebSocket.Server({
 
 app.use(`${path}/`, express.static("build"));
 app.use(`${path}/favicon.png`, express.static("build/favicon.png"));
-
-app.get(`${path}/api/apps`, async (req, res) => {
-  try {
-    const response = await fetch("http://127.0.0.1:5000/api/apps");
-    const json = await response.json();
-    res.send(json);
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-
-app.get(`${path}/api/settings`, async (req, res) => {
-  try {
-    const response = await fetch("http://127.0.0.1:5000/api/settings");
-    const json = await response.json();
-    res.send(json);
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-
-app.post(`${path}/api/app/state/:app/disable`, async (req, res) => {
-  try {
-    await fetch(
-      `http://127.0.0.1:5000/api/app/state/${req.params.app}/disable`,
-      { method: "post", data: {} }
-    );
-    res.json({});
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-
-app.post(`${path}/api/app/state/:app/enable`, async (req, res) => {
-  try {
-    await fetch(
-      `http://127.0.0.1:5000/api/app/state/${req.params.app}/enable`,
-      { method: "post", data: {} }
-    );
-    res.json({});
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
 
 let frontendWS;
 
