0cbb5d966bd3dff2c95b51db8382349bd247354d
Ludeeus <ludeeus@ludeeus.dev>
2020-08-17 20:31:04 +0000

    Add retry/reconnect

--- host/admin.js
+++ host/admin.js
@@ -6,7 +6,8 @@ const server = require("http").createServer();
 const app = express();
 const path = process.env.ADMINPATH || "";
 const port = 1337;
-const NetDaemonWS = new WebSocket("ws://127.0.0.1:5000/api/ws");
+let NetDaemonWS;
+let frontendWS;
 
 let wss = new WebSocket.Server({
   server: server,
@@ -15,22 +16,45 @@ let wss = new WebSocket.Server({
 app.use(`${path}/`, express.static("build"));
 app.use(`${path}/favicon.png`, express.static("build/favicon.png"));
 
-let frontendWS;
-
 wss.on("connection", function connection(ws) {
   frontendWS = ws;
   ws.on("message", function incoming(message) {
     const _message = JSON.parse(message);
     console.log(`received message from frontend:`, _message.type);
-    NetDaemonWS.send(message);
+    if (NetDaemonWS) {
+      NetDaemonWS.send(message);
+    }
   });
 });
 
-NetDaemonWS.on("message", function incoming(message) {
-  const _message = JSON.parse(message);
-  console.log(`received message from NetDaemon:`, _message.type);
-  frontendWS.send(message);
-});
+function connect() {
+  var ws = new WebSocket("ws://127.0.0.1:5000/api/ws");
+  ws.onopen = function () {
+    NetDaemonWS = ws;
+    server.on("request", app);
+    server.listen(port, () => console.log(`Running NetDaemon Admin`));
+  };
 
-server.on("request", app);
-server.listen(port, () => console.log(`Running NetDaemon Admin`));
+  ws.on("message", function incoming(message) {
+    const _message = JSON.parse(message);
+    console.log(`received message from NetDaemon:`, _message.type);
+    frontendWS.send(message);
+  });
+
+  ws.onclose = function (e) {
+    console.log(
+      "Socket is closed. Reconnect will be attempted in 1 second.",
+      e.reason
+    );
+    setTimeout(function () {
+      connect();
+    }, 1000);
+  };
+
+  ws.onerror = function (err) {
+    console.error("Socket encountered error: ", err.message, "Closing socket");
+    ws.close();
+  };
+}
+
+connect();
